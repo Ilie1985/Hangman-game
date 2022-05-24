4cde1226f51b92daebb0c5e83fd0cd973a73fecd
Marian Ilie <ilie.marian32@yahoo.com>
2022-05-24 14:36:53 +0100

    wrong letters and play again functionality

--- script.js
+++ script.js
@@ -99,10 +99,48 @@ displayWord();
 
 //updateTheWrongLetterEl FUNCTIONALITY
 
-const updateTheWrongLettersEl = () => {};
+//get the wrongLettersEl from the DOM and add to the innerHTML
+//check if the length of the array worngLetters is greater than 0
+//use ternary operator and if there is something in the array put a paragraph that says wrong
+//if its empty give back an empty string
+//store the letters in the wrongaletters array and map through
+//for each letter output a span with the letter
+//loop through figureParts with forEach
+//set part and index param
+//see how many missed letters(errors) i have with wrongLetters.length
+//each part has in index starting at 0
+//if index is less than errors  take part and style it do be displayed block (by default is display none)
+//else, part is styled to be displayed none
+// the game is lost if the number of wrongLetters.length === figureParts.length
+// set the innerText of the finalMessage
+//display the popup and style it to be flex
+const updateWrongLettersEl = () => {
+  //Display wrong letters
+  wrongLettersEl.innerHTML = ` ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+     ${wrongLetters.map((letter) => `<span>${letter}</span>`)} `;
+
+  //Display parts
+  figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length;
+
+    if (index < errors) {
+      part.style.display = "block";
+    } else {
+      part.style.display = "none";
+    }
+  });
+
+  //Check if game is lost
+
+  if (wrongLetters.length === figureParts.length) {
+    finalMessage.innerText = "Unfortunately you lost. 😕";
+    popup.style.display = "flex";
+  }
+};
 //========================================================================
 
 // showNotification FUNCTIONALITY
+
 //add the show class to notification element
 //set it to dissapear automatically after 2sec
 //remove the show class
@@ -129,12 +167,16 @@ const showNotification = () => {
 //if the letter is not included in selectedWord push letter to the wrongLetters array
 //make sure it is not already on the wrongLetters array with if statement
 //invoke the function updateWrongLetterEl to be displayed in the wrongLettersElement
+// keyCode is deprecated ,could not find an alternative for it for now
+
 window.addEventListener("keydown", (e) => {
-  if (e.key >= 65 && e.key <= 90) {
+  if (e.keyCode >= 65 && e.keyCode <= 90) {
     const letter = e.key;
+
     if (selectedWord.includes(letter)) {
       if (!correctLetters.includes(letter)) {
         correctLetters.push(letter);
+
         displayWord();
       } else {
         showNotification();
@@ -148,5 +190,25 @@ window.addEventListener("keydown", (e) => {
       }
     }
   }
+});
+
+// Restart game and play again
+//Empty the correctLetters and wrongLetters arrays
+//splice to 0
+//get a new random word from the array words
+//call displayWord()
+//cleanup the wrong letters and hide the parts (the figurine) by calling updateWrongLettersEl
+//hide the popup
+playAgainBtn.addEventListener("click", () => {
+  correctLetters.splice(0);
+  wrongLetters.splice(0);
+
+  selectedWord = words[Math.floor(Math.random() * words.length)];
+
+  displayWord();
+
+  updateWrongLettersEl();
+
+  popup.style.display = "none";
 });
 //=====================================================================================
